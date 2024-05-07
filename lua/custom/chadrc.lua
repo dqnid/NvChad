8445ae77e958faa8d452e16c391c12fe89f214ff
dqnid <dani.heras@hotmail.com>
2024-05-07 13:27:28 +0200

    Commander added with basic config + floating commands and notifications

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -6,8 +6,8 @@ local highlights = require("custom.highlights")
 
 M.ui = {
 
-	theme = "pastelbeans",
-	theme_toggle = { "pastelbeans", "penumbra_light" },
+	theme = "penumbra_light",
+	theme_toggle = { "penumbra_light", "penumbra_light" },
 
 	hl_override = highlights.override,
 	hl_add = highlights.add,
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,8 +2,45 @@
 -- List of all default plugins & their definitions
 local default_plugins = {
 	"shaunsingh/nord.nvim",
-
 	"nvim-lua/plenary.nvim",
+
+	"MunifTanjim/nui.nvim",
+	"rcarriga/nvim-notify",
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add any options here
+		},
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+		config = function()
+			require("noice").setup({
+				lsp = {
+					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+					},
+				},
+				presets = {
+					bottom_search = true, -- use a classic bottom cmdline for search
+					command_palette = true, -- position the cmdline and popupmenu together
+					long_message_to_split = true, -- long messages will be sent to a split
+					inc_rename = false, -- enables an input dialog for inc-rename.nvim
+					lsp_doc_border = false, -- add a border to hover docs and signature help
+				},
+			})
+		end,
+	},
+
 	{
 		"rmagatti/session-lens",
 		requires = { "rmagatti/auto-session", "nvim-telescope/telescope.nvim" },
@@ -103,6 +140,9 @@ local default_plugins = {
 					},
 				},
 			})
+		end,
+		opts = function()
+			return require("plugins.configs.commander")
 		end,
 	},
 	{
