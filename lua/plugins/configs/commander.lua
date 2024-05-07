f1c3ab79f0f573118583ddbf27919a9c80e780c8
dqnid <dani.heras@hotmail.com>
2024-05-07 13:31:02 +0200

    configuration moved to commander.lua

--- lua/plugins/configs/commander.lua
+++ lua/plugins/configs/commander.lua
@@ -1,5 +1,28 @@
 local commander = require("commander")
 
+commander.setup({
+	components = {
+		"DESC",
+		"KEYS",
+		"CAT",
+	},
+	sort_by = {
+		"DESC",
+		"KEYS",
+		"CAT",
+		"CMD",
+	},
+	integration = {
+		telescope = {
+			enable = true,
+		},
+		lazy = {
+			enable = true,
+			set_plugin_name_as_cat = true,
+		},
+	},
+})
+
 commander.add({
 	{
 		desc = "Search inside current buffer",
